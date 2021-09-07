1f22f3963ad9642666a79d1b7ccf5c06d97f8e6e
Ninad Kalanke <ninadkalanke@gmail>
2021-09-07 17:46:20 +0530

    Server side changes

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -4,12 +4,21 @@ export const getPosts = async (req,res) => {
     try {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
-        
-    } catch(error) {
+
+    } catch (error) {
         res.status(error.status).json({ message : error.message });
     }
 };
 
 export const createPost = async (req,res) => {
-    res.send('Creation of Post.');
+    const body = req.body;
+    
+    const newPost = new PostMessage(body);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(error.status).json({ message : error.message });
+    }
 }
