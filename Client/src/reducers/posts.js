ff065396acca16a6137b6793425f5e365385f9e7
Ninad Kalanke <ninadkalanke@gmail>
2021-09-10 18:58:12 +0530

    Some Redux work

---
+++ Client/src/reducers/posts.js
@@ -0,0 +1,15 @@
+const reducer = (state = [], action) => {
+
+    switch (action.type) {
+        case "FETCH_POSTS":
+            return state;
+
+        case "CREATE":
+            return state;
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
