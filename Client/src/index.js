741a254abb27d00a6e411394fc024072e95a54e4
Ninad Kalanke <ninadkalanke@gmail>
2021-09-10 18:22:23 +0530

    Redux started

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react";
+import { createStore, applymiddleware, compose } from "redux";
+import thunk from "react-thunk";
 
-import App from './App';
+import reducers from "./reducers";
+
+import App from "./App";
+
+const store = createStore(reducers, compose(applymiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById('root'));
