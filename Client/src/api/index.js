ea6ee40130506f67e9616b5b216f5efc5354f8f8
Ninad Kalanke <ninadkalanke@gmail.com>
2021-11-03 18:13:34 +0530

    Authentication complete but like edit delete restrictions not

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -3,12 +3,23 @@ import axios from "axios";
 const url = "http://localhost:5000";
 // const url = "https://momory-react.herokuapp.com";
 
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const createPost = (newPost) => axios.post(`${url}/posts`, newPost);
-export const updatePost = (id, post) => axios.patch(`${url}/posts/${id}`, post);
-export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
-export const likePost = (id) => axios.patch(`${url}/posts/${id}/likepost`);
+const API = axios.create({ baseURL: url });
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile'))
+    {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
+
+export const fetchPosts = () => API.get(`/posts`);
+export const createPost = (newPost) => API.post(`/posts`, newPost);
+export const updatePost = (id, post) => API.patch(`/posts/${id}`, post);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likepost`);
 
 
-export const userLogin = (result) => axios.post(`${url}/user/login`, result);
-export const userSignup = (result) => axios.post(`${url}/user/signup`, result);
+export const userLogin = (result) => API.post(`/user/login`, result);
+export const userSignup = (result) => API.post(`/user/signup`, result);
--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     
     try {
 
-        const token = req.headers.authorization?.spilt(' ')[1];
+        const token = req.headers.authorization?.split(' ')[1];
         
         if(token)
         {
